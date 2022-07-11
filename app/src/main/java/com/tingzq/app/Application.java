ca648a15d99f120a4d7118fe38a19a8a7427abdf
TingZQ <qxj1996@126.com>
2022-07-11 22:26:28 +0800

    优化启动时间

--- app/src/main/java/com/tingzq/app/Application.java
+++ app/src/main/java/com/tingzq/app/Application.java
@@ -1,8 +1,12 @@
 package com.tingzq.app;
 
 
+import android.content.Context;
+
+import com.bytedance.boost_multidex.BoostMultiDex;
 import com.tencent.mmkv.BuildConfig;
 import com.tingzq.base.base.BaseApplication;
+import com.tingzq.common.config.ModuleLifecycleConfig;
 
 public class Application extends BaseApplication {
 
@@ -10,5 +14,15 @@ public class Application extends BaseApplication {
     public void onCreate() {
         super.onCreate();
         setsDebug(BuildConfig.DEBUG);
+
+        // 初始化需要初始化的组件
+        ModuleLifecycleConfig.getInstance().initModuleAhead(this);
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+
+        BoostMultiDex.install(base);
     }
 }
